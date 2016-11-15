f3c993c834967c774d784b1dc9f314d8c244c591
tithi021 <afroza021@gmail.com>
2016-11-15 15:55:20 +0600

    Separate components for header main body and footer

--- App.jsx
+++ App.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Hello React Developers!!!</h1>
-         </div>
-      );
-   }
+	 render() {
+			 
+			return (
+				 <div>
+				 		<Header/>
+						<Content/>
+						<Footer/>
+				 </div>
+			);
+	 }
 }
 
+class Header extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Header</h1>
+			</div>
+		);
+	}
+}
+class Content extends React.Component {
+	render() {
+		var myStyle = {
+				fontSize: 20,
+				color: '#ff0000'
+		}
+		return (
+			<div>
+				<h1 style= {myStyle} >Hello React Developers!!!</h1>
+			</div>
+		);
+	}
+}
+class Footer extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2>Footer</h2>
+			</div>
+		);
+	}
+}
 export default App;
